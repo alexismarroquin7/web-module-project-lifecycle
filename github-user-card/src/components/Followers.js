24c2ff60d725f3ff74487561ff83e4f04b31d829
Alexis Marroquin <alexismarroquin754@gmail.com>
2021-03-09 16:01:39 -0800

    style follower card

--- github-user-card/src/components/Followers.js
+++ github-user-card/src/components/Followers.js
@@ -1,4 +1,18 @@
 import React from "react"
+import {withStyles, Card, Grid} from "@material-ui/core"
+
+const useStyles = theme => ({
+    root: {
+        width:"100%",
+        borderRadius: "10px",
+        marginTop: "3rem",
+        marginBottom: "3rem"
+    },
+    card: {
+        border: "1px solid #242424",
+        width:"30%"
+    }
+})
 
 class Followers extends React.Component {
     componentDidMount(){
@@ -7,15 +21,18 @@ class Followers extends React.Component {
     }
     
     render(){
+        const {classes} = this.props
         return(
         
-        <div>
+        <Grid className={classes.root}>
+            <Card className={classes.card}>
             <h2>{this.props.follower.login}</h2>
             <h2>{this.props.follower.html_url}</h2>
             <img alt={this.props.follower.login} src={this.props.follower.avatar_url} />
-        </div>
+            </Card>
+        </Grid>
         )
     }
 }
 
-export default Followers;
+export default withStyles(useStyles)(Followers);
